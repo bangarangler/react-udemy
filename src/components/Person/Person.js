4006ee24d74f45335e7a3fac1f92612202989ad0
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-02-20 11:37:48 -0500

    Different ways of using Context to move data around without prop drilling

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -4,17 +4,39 @@ import Aux from "../../hoc/Aux";
 import WithClass from "../../hoc/WithClass";
 import PropTypes from "prop-types";
 
+import AuthContext from "../../context/auth-context";
+
 class Person extends Component {
+  constructor(props) {
+    super(props);
+    this.inputElementRef = React.createRef();
+  }
+
+  static contextType = AuthContext;
+
+  componentDidMount() {
+    //this.inputElement.focus();
+    this.inputElementRef.current.focus();
+    console.log(this.context.authenticated);
+  }
   render() {
     console.log("[Person.js] rendering...");
     return (
       <Aux>
         <React.Fragment>
+          {this.context.authenticated ? (
+            <p>Authenticated!</p>
+          ) : (
+            <p>Please log in</p>
+          )}
+          ) }
           <p onClick={this.props.click}>
             I'm {this.props.name} and I am {this.props.age} years old!
           </p>
           <p>{this.props.children}</p>
           <input
+            //ref={inputEl => { this.inputElement = inputEl; }}
+            ref={this.inputElementRef}
             type="text"
             onChange={this.props.changed}
             value={this.props.name}
