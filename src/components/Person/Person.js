8f152f45004f866c00b5ac274df7c1aa9f2e9d9b
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-02-20 09:06:23 -0500

    Added Prop-types

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -1,23 +1,35 @@
 import React, { Component } from "react";
 import classes from "./Person.css";
+import Aux from "../../hoc/Aux";
+import WithClass from "../../hoc/WithClass";
+import PropTypes from "prop-types";
 
 class Person extends Component {
   render() {
     console.log("[Person.js] rendering...");
     return (
-      <div className={classes.Person}>
-        <p onClick={this.props.click}>
-          I'm {this.props.name} and I am {this.props.age} years old!
-        </p>
-        <p>{this.props.children}</p>
-        <input
-          type="text"
-          onChange={this.props.changed}
-          value={this.props.name}
-        />
-      </div>
+      <Aux>
+        <React.Fragment>
+          <p onClick={this.props.click}>
+            I'm {this.props.name} and I am {this.props.age} years old!
+          </p>
+          <p>{this.props.children}</p>
+          <input
+            type="text"
+            onChange={this.props.changed}
+            value={this.props.name}
+          />
+        </React.Fragment>
+      </Aux>
     );
   }
 }
 
-export default Person;
+Person.propTypes = {
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  changed: PropTypes.func
+};
+
+export default WithClass(Person, classes.Person);
--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 import classes from "./App.css";
 import Persons from "../components/Persons/Persons";
 import Cockpit from "../components/Cockpit/Cockpit";
+import WithClass from "../hoc/WithClass";
 
 class App extends Component {
   constructor(props) {
@@ -18,7 +19,8 @@ class App extends Component {
     ],
     otherState: "some other value",
     showPersons: false,
-    showCockpit: true
+    showCockpit: true,
+    changeCounter: 0
   };
 
   static getDerivedStateFromProps(props, state) {
@@ -60,7 +62,12 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState({ persons: persons });
+    this.setState((prevState, props) => {
+      return {
+        persons: persons,
+        changeCounter: prevState.changeCounter + 1
+      };
+    });
   };
 
   deletePersonsHandler = personIndex => {
@@ -90,7 +97,7 @@ class App extends Component {
     }
 
     return (
-      <div className={classes.App}>
+      <React.Fragment>
         <button
           onClick={() => {
             this.setState({ showCockpit: false });
@@ -107,9 +114,9 @@ class App extends Component {
           />
         ) : null}
         {persons}
-      </div>
+      </React.Fragment>
     );
   }
 }
 
-export default App;
+export default WithClass(App, classes.App);
